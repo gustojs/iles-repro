c3f7bd0b4d194fbe92d246dcc5f37c0122438f5a
gustojs <gustojs@protonmail.com>
2021-10-20 15:03:29 +0200

    initial commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,12 @@
+<template>
+  <button @click="click">click me</button>
+  <div>counter is now {{ counter }}</div>
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+const counter = ref(0);
+function click() {
+  counter.value++;
+}
+</script>
